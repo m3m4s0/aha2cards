2209c74f0159423a2f3a4b2fdfdd5a4963cff0c5
Malte <malte.sohns@googlemail.com>
2019-04-07 19:48:10 +0200

    Inital Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aha2cards
+This project is still a work in progress
+
+This tool helps you to export your AHA.IO Features or Ideas in a printable format to use in on hands planning sessions
+
+## Next steps
+* include export files
+* direct connection to AHA.IO API to pull information
---
+++ cards.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import jinja2
+import pdfkit
+
+
+def read_dataset():
+    df = pd.read_csv('features.csv')
+    dfi = pd.read_csv('ideas.csv')
+    cards = df.append(dfi, sort=False)
+    cards['Feature_description'] = cards['Feature_description'].str.slice(0,180) + '...' 
+    template_vars = {'cards' : cards}
+    prepare_template(template_vars, 'cards')
+
+def prepare_template(template_vars, template):
+    templateLoader = jinja2.FileSystemLoader(searchpath="./template")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = template+".html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    outputText = template.render(template_vars)
+    write_to_html(outputText)
+
+
+def write_to_html(outputText):    
+    html_file = open('reports/cards.html', 'w', encoding='utf-8')
+    html_file.write(outputText)
+    html_file.close()
+    
+
+read_dataset()
