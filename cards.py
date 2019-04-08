625c2306af994c2c2f19bd9ba6e83b99353fcb38
Malte <malte.sohns@googlemail.com>
2019-04-08 20:33:52 +0200

    added argeparse to script

--- cards.py
+++ cards.py
@@ -4,25 +4,25 @@ import jinja2
 import requests 
 import tqdm
 import configparser
+import argparse
 
 def get_from_api(company, api_key, product,status_to_ignore):
     api_url = f'https://{company}.aha.io/api/v1/products/{product}/features'
     response = requests.get(f'{api_url}', headers={'Authorization': f'Bearer {api_key}'})
     pages = response.json()['pagination']['total_pages']
     features = response.json()['features']
-    # for page in tqdm.tqdm(range(2,pages+1)):
-    for page in tqdm.tqdm(range(2,3)):        
+    for page in tqdm.tqdm(range(2,pages+1)):  
         response = requests.get(f'{api_url}?page={page}', headers={'Authorization': f'Bearer {api_key}'})
         features.extend(response.json()['features'])
     
-    f_list = []
+    feature_list = []
     for f_id in tqdm.tqdm(features):
         response = requests.get(f'{api_url}/{f_id["id"]}', headers={'Authorization': f'Bearer {api_key}'})      
         feature = response.json()['feature']
-        if feature['workflow_status']['name'].upper() not in status_to_ignore: # this is ugly since the API doesn't allow query against the status
-            f_list.append(feature)  
+        if feature['workflow_status']['name'].upper() not in status_to_ignore: # this is fugly; aha! doesn't support an option to call features by status
+            feature_list.append(feature)  
     
-    template_vars = {'cards' : f_list}
+    template_vars = {'cards' : feature_list}
     outtext = prepare_template(template_vars, 'cards_api')   
     write_to_html(outtext, 'cards_api')     
 
@@ -54,12 +54,17 @@ def write_to_html(outputText, fileName):
 
 
 if __name__ == "__main__":
-    # read_dataset()
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    company = config['AHA.IO']['COMPANY']
-    api_key = config['AHA.IO']['API_KEY']
-    product = config['AHA.IO']['PRODUCT']
-    status_to_ignore = config['AHA.IO']['STATUS_TO_IGNORE']
-    get_from_api(company,api_key, product,status_to_ignore)
-
+    parser = argparse.ArgumentParser(description='Report Tool to gather Information from the DCGY API.')
+    parser.add_argument('-api','--api',  action='store_true')
+    parser.add_argument('-f', '--inFile', action='store_true')
+    args = parser.parse_args()
+    if args.api:
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        company = config['AHA.IO']['COMPANY']
+        api_key = config['AHA.IO']['API_KEY']
+        product = config['AHA.IO']['PRODUCT']
+        status_to_ignore = config['AHA.IO']['STATUS_TO_IGNORE']
+        get_from_api(company,api_key, product,status_to_ignore)
+    if args.inFile:
+        read_dataset()
